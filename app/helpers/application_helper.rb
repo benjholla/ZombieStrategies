10d547f8ac3f6e60a1f3dbc62a05cff27bad9606
Ben Holland <benjholla@benjholla.local>
2009-07-28 18:15:55 -0500

    css fixes, comibined general layout files, google maps fixes, general code cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def page_title
+    case controller.controller_name
+    when 'stores'
+      'Plan'
+    when 'posts'
+      'Announcements'
+    when 'twitter_computations'
+      'Zombie Twitter Computation'
+    when 'twitter_trends'
+      'Zombie Twitter Trends'
+    else
+      'Zombie Strategies'
+    end
+  end
+  
 end
