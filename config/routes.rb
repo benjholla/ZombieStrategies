1ff4737ce8df9a2390ec67123edec346a3db6aa2
Ben Holland <benjholla@benjholla.local>
2009-07-28 20:27:49 -0500

    fixed some maps controller issues, refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :stores
+  map.resources :stores, :collection => {:create => :get}
   map.resources :homes, :has_many => :posts
   map.resources :posts
   map.resources :twitter_trends, :has_many => :twitter_computations
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action'
+  #map.connect ':controller/:action'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
