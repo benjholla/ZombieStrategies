8c63efa8e98842bf15c0ecd1f2b9db050ac3338e
Ben Holland <benjholla@benjholla.(none)>
2009-09-08 18:17:28 -0500

    added about and store pages

---
+++ app/helpers/information_controller_helper.rb
@@ -0,0 +1,2 @@
+module InformationControllerHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -50,5 +50,6 @@ ActionController::Routing::Routes.draw do |map|
   #map.connect ':controller/:action'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect ':action.:format', :controller => 'information'
 
 end
