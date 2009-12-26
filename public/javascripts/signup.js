10c8aa865db1012093dc12453866a4b25f7bea91
Ben Holland <benjholla@benjholla.local>
2009-12-25 23:51:22 -0600

    add the search bar for geolocation to the signup page, modified code to place a marker at the resulting query

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -13,12 +13,162 @@ oFormObject = document.forms[0];
 //latFormElement = oFormObject.elements["user_lat"];
 //lngFormElement = oFormObject.elements["user_lng"];
 
+// ******************************* START GEOCODER HELPER FUNCTIONS *******************************
+
+// ====== Create a Client Geocoder ======
+var geo = new GClientGeocoder(); 
+
+// ====== Array for decoding the failure codes ======
+var reasons=[];
+reasons[G_GEO_SUCCESS]            = "Success";
+reasons[G_GEO_MISSING_ADDRESS]    = "Missing Address: The address was either missing or had no value.";
+reasons[G_GEO_UNKNOWN_ADDRESS]    = "Unknown Address:  No corresponding geographic location could be found for the specified address.";
+reasons[G_GEO_UNAVAILABLE_ADDRESS]= "Unavailable Address:  The geocode for the given address cannot be returned due to legal or contractual reasons.";
+reasons[G_GEO_BAD_KEY]            = "Bad Key: The API key is either invalid or does not match the domain for which it was given";
+reasons[G_GEO_TOO_MANY_QUERIES]   = "Too Many Queries: The daily geocoding quota for this site has been exceeded.";
+reasons[G_GEO_SERVER_ERROR]       = "Server error: The geocoding request could not be successfully processed.";
+
+// ===== list of words to be standardized =====
+var standards = [   ["road","rd"],   
+                    ["street","st"], 
+                    ["avenue","ave"], 
+                    ["av","ave"], 
+                    ["drive","dr"],
+                    ["saint","st"], 
+                    ["north","n"],   
+                    ["south","s"],    
+                    ["east","e"], 
+                    ["west","w"],
+                    ["expressway","expy"],
+                    ["parkway","pkwy"],
+                    ["terrace","ter"],
+                    ["turnpike","tpke"],
+                    ["highway","hwy"],
+                    ["lane","ln"]
+                 ];
+
+// ===== convert words to standard versions =====
+function standardize(a) {
+  for (var i=0; i<standards.length; i++) {
+    if (a == standards[i][0])  {a = standards[i][1];}
+  }
+  return a;
+}
+
+// ===== check if two addresses are sufficiently different =====
+function different(a,b) {
+  // only interested in the bit before the first comma in the reply
+  var c = b.split(",");
+  b = c[0];
+  // convert to lower case
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+  // remove apostrophies
+  a = a.replace(/'/g ,"");
+  b = b.replace(/'/g ,"");
+  // replace all other punctuation with spaces
+  a = a.replace(/\W/g," ");
+  b = b.replace(/\W/g," ");
+  // replace all multiple spaces with a single space
+  a = a.replace(/\s+/g," ");
+  b = b.replace(/\s+/g," ");
+  // split into words
+  awords = a.split(" ");
+  bwords = b.split(" ");
+  // perform the comparison
+  var reply = false;
+  for (var i=0; i<bwords.length; i++) {
+    //GLog.write (standardize(awords[i])+"  "+standardize(bwords[i]))
+    if (standardize(awords[i]) != standardize(bwords[i])) {reply = true}
+  }
+  //GLog.write(reply);
+  return (reply);
+}
+
+
+function getFormattedLocation() {
+  if (google.loader.ClientLocation.address.country_code == "US" &&
+    google.loader.ClientLocation.address.region) {
+    return google.loader.ClientLocation.address.city + ", " 
+        + google.loader.ClientLocation.address.region.toUpperCase();
+  } else {
+    return  google.loader.ClientLocation.address.city + ", "
+        + google.loader.ClientLocation.address.country_code;
+  }
+}
+
+function showExactAddress(address) {
+  document.getElementById("message").innerHTML = "";
+  document.getElementById("search").value = address;
+  showAddress();
+}
+
+  // ====== Geocoding ======
+  function showAddress() {
+	// clear out old markers
+	clearMarkers();
+	// get search query value
+    var search = document.getElementById("search").value;
+    // ====== Perform the Geocoding ======        
+    geo.getLocations(search, function (result)
+    { 
+        // If that was successful
+        if (result.Status.code == G_GEO_SUCCESS) {
+		 	// ===== If there was more than one result, "ask did you mean" on them all =====
+		 	if (result.Placemark.length > 1) { 
+				document.getElementById("message").innerHTML = "Did you mean:";
+				// Loop through the results
+				for (var i=0; i<result.Placemark.length; i++) {
+			   		var p = result.Placemark[i].Point.coordinates;
+			   		document.getElementById("message").innerHTML += "<br>"+(i+1)+": <a href='javascript:showExactAddress(\"" + result.Placemark[i].address + "\")'>" + result.Placemark[i].address + "<\/a>";
+		    	}
+		 	}
+		 	// ===== If there was a single marker, check if the returned address significantly different =====
+		 	else 
+		 	{
+		 		document.getElementById("message").innerHTML = "";
+				if (different(search, result.Placemark[0].address)) {
+					document.getElementById("message").innerHTML = "Did you mean: ";
+			    	var p = result.Placemark[0].Point.coordinates;
+			    	document.getElementById("message").innerHTML += "<a href='javascript:showExactAddress(\"" + result.Placemark[0].address +"\")'>"+ result.Placemark[0].address+"<\/a>";
+				}
+			 	else
+			 	{
+			    	var p = result.Placemark[0].Point.coordinates;
+					// ===== Look for the bounding box of the first result =====
+		          	var N = result.Placemark[0].ExtendedData.LatLonBox.north;
+		          	var S = result.Placemark[0].ExtendedData.LatLonBox.south;
+		          	var E = result.Placemark[0].ExtendedData.LatLonBox.east;
+		          	var W = result.Placemark[0].ExtendedData.LatLonBox.west;
+		          	var bounds = new GLatLngBounds(new GLatLng(S,W), new GLatLng(N,E));
+		          	// Choose a zoom level that fits
+		          	var zoom = map.getBoundsZoomLevel(bounds);
+		          	map.setCenter(bounds.getCenter(),zoom);
+					// add a marker at the center bounds
+					setMarker(bounds.getCenter());
+		  	 	}
+		  	}
+        }
+        // ====== Decode the error status ======
+        else {
+          	var reason="Code " + result.Status.code;
+          	if (reasons[result.Status.code]) {
+            	reason = reasons[result.Status.code]
+          	}
+ 			document.getElementById("message").innerHTML = 'Could not find "' + search + '"<br />' + '<font color="red">' + reason + '</font>';
+        }
+      }
+    );
+  }
+
+// ******************************* END GEOCODER HELPER FUNCTIONS *******************************
+
 function init() {
   	if (GBrowserIsCompatible()) {
     	map = new GMap2(document.getElementById("map"));
 	
 		// used to store a resulting message from the geocoder
-		//var location = "";
+		var location = "";
 		
 	    // If ClientLocation was filled in by the loader, use that info instead
 	    if (google.loader.ClientLocation) {
@@ -26,11 +176,14 @@ function init() {
 	    	centerLatitude = google.loader.ClientLocation.latitude;
 			centerLongitude = google.loader.ClientLocation.longitude;
 			setMarker(new google.maps.LatLng(centerLatitude, centerLongitude));
-	    	//location = "Showing IP-based location: <b>" + getFormattedLocation() + "</b>";
+	    	location = "Showing IP-based location: <b>" + getFormattedLocation() + "</b>";
 	    }
 	
 		// sets the map to the default position listed above, or the IP geocododed location if found
 		map.setCenter(new GLatLng(centerLatitude, centerLongitude), startZoom);
+		
+		// update the location div element
+		document.getElementById("message").innerHTML = location;
 
 		// add some nice controls to the map
 		map.addControl(new GLargeMapControl());
