3d87c1c39aa8a8ff02224776a1d46e455c4cc1ca
Ben Holland <benjholla@benjholla.local>
2009-12-30 21:31:34 -0600

    added a task to add a default user 'admin' with default password 'badpass', when users database is created

--- db/migrate/20091224215842_create_users.rb
+++ db/migrate/20091224215842_create_users.rb
@@ -20,6 +20,14 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps 
     end
     add_index :users, :login, :unique => true
+    @user = User.create(:login=>"admin", :password=>"***REMOVED***", :password_confirmation=>"***REMOVED***", :first_name=>"Ben", :last_name=>"Holland", :email=>"***REMOVED***@gmail.com", :phone=>"***REMOVED***", :twitter=>"***REMOVED***", :lat=>"***REMOVED***", :lng=>"***REMOVED***")
+    # reset the is_admin property for new users to false just incase someone crafts a form
+    @user.is_admin = 1
+    if @user && @user.save
+      puts "Success: created new admin user with default password = ***REMOVED***"
+    else
+      puts "Error: could not create default user!"
+    end
   end
 
   def self.down
