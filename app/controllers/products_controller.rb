52ae633fca3624eae64f0e2ba23c734ddf1c0260
Ben Holland <benjholla@benjholla.local>
2010-01-05 23:21:34 -0600

    removed some junk files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product.save
         flash[:notice] = 'Product was successfully created.'
-        format.html { redirect_to(@product) }
+        format.html { redirect_to(categories_url) }
         format.xml  { render :xml => @product, :status => :created, :location => @product }
       else
         format.html { render :action => "new" }
