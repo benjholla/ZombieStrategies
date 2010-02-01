f405aa0d02118b05bc4f691e23d5d264a545a08f
Ben Holland <benjholla@benjholla.(none)>
2010-02-01 01:48:17 -0600

    temp fix

--- app/controllers/location_profiles_controller.rb
+++ app/controllers/location_profiles_controller.rb
@@ -55,7 +55,7 @@ class LocationProfilesController < ApplicationController
 
   # GET /location_profiles/1/edit
   def edit
-    @location_profile = LocationProfile.find(params[:id])
+    @location_profile = LocationProfile.find_by_name(params[:id])
   end
 
   # POST /location_profiles
@@ -80,7 +80,7 @@ class LocationProfilesController < ApplicationController
   # PUT /location_profiles/1
   # PUT /location_profiles/1.xml
   def update
-    @location_profile = LocationProfile.find_by_name(params[:id])
+    @location_profile = LocationProfile.find(params[:id])
     
     respond_to do |format|
       if @location_profile.update_attributes(params[:location_profile])
