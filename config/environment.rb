623119aed794a13052e46f57e6847845da659fc5
Ben Holland <benjholla@amentis.local>
2010-12-26 21:20:53 -0500

    updated pdf generation for locations

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'will_paginate', :version => '~> 2.3.11'
   config.gem 'gchart', :version => '~> 1.0.0'
+  config.gem 'prawn'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
